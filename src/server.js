f95b469b46a0948f2225814d6cbd3da40c4ec7ff
dhino rahmad <dhinorahmad0@gmail.com>
2021-04-29 10:18:39 +0700

    fix bugs sub-domain value

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const routes = require('./routes');
 const init = async () => {
     const server = Hapi.server({
         port: 5000,
-        host: 'localhost',
+        host: process.env.PORT || 5000,
         routes: {
             cors: {
                 origin: ['*'],

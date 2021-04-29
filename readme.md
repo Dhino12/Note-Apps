9b34b44d1a472cc413c2de9c3f219f865a6c56d6
dhino rahmad <dhinorahmad0@gmail.com>
2021-04-29 09:39:55 +0700

    Simple Note API

---
+++ readme.md
@@ -0,0 +1,12 @@
+[UI Testing](http://ec2-13-212-153-62.ap-southeast-1.compute.amazonaws.com:8000/) 
+
+## Tools Used
+- [Nodemon](https://www.npmjs.com/package/nodemon) // dev
+- [Eslint](https://eslint.org/docs/user-guide/getting-started) // dev
+
+## Dependecies Used
+- [Hapi](https://hapi.dev/tutorials/?lang=en_US) 
+- [NanoID](https://github.com/ai/nanoid) 
+
+## Code Style Used
+- [AirBnB](https://github.com/airbnb/javascript)
---
+++ src/routes.js
@@ -0,0 +1,40 @@
+/* eslint-disable indent */
+/* I turned off the airBnB indentation because it was too weird for me */
+
+const {
+    addNoteHandler,
+    getAllNotesHandler,
+    getNotesByIdHandler,
+    editNoteByIdHandler,
+    deleteNoteByIdHandler,
+} = require('./handler');
+
+const routes = [
+    {
+        method: 'POST',
+        path: '/notes',
+        handler: addNoteHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes',
+        handler: getAllNotesHandler,
+    },
+    {
+        method: 'GET',
+        path: '/notes/{id}',
+        handler: getNotesByIdHandler,
+    },
+    {
+        method: 'PUT',
+        path: '/notes/{id}',
+        handler: editNoteByIdHandler,
+    },
+    {
+        method: 'DELETE',
+        path: '/notes/{id}',
+        handler: deleteNoteByIdHandler,
+    },
+];
+
+module.exports = routes;
